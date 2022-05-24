e04015c1fbc7b365e848b801da293bbc6258373d
Yevhen <104986485+YevhenVovk1992@users.noreply.github.com>
2022-05-24 15:30:04 +0300

    Delete HomeTask.py
    Error

--- HomeTask.py
+++
@@ -1,7 +0,0 @@
-#First answer
-input_str = input('Please, input three-digit number: ')
-print('Inversion: ', input_str[::-1])
-
-#second answer
-backwards = input_str[-1]+input_str[-2]+input_str[-3]
-print('Inversion: ', backwards)
