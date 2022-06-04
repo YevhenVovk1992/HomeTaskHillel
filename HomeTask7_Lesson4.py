e768c33e527e8b1142181030b4395c440da345ea
Yevhen <104986485+YevhenVovk1992@users.noreply.github.com>
2022-06-04 14:43:07 +0300

    ДЗ 7. Подсчет предложений в тексте
    bug fix

--- HomeTask7_Lesson4.py
+++ HomeTask7_Lesson4.py
@@ -1,5 +1,5 @@
 USER_INPUT = input("Write text here: ")
 N = USER_INPUT.count(".")
-if USER_INPUT.rfind(".") < len(USER_INPUT)-1:
+if USER_INPUT and USER_INPUT[-1] != '.':
     N += 1
 print(f'You wrote {N} sentences.')
