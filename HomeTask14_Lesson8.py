397b2d993d9f0a86d3f10ff03bf1119fbae8cc5b
Yevhen <104986485+YevhenVovk1992@users.noreply.github.com>
2022-06-17 15:32:23 +0300

    ДЗ 14. Поиск квадратного корня бинарным поиском

---
+++ HomeTask14_Lesson8.py
@@ -0,0 +1,28 @@
+num = int(input('Number: '))
+
+def square_root(x):
+    if x < 2:
+        return x
+    result = 0
+    left = 0
+    right = x
+    while left <= right:
+        
+        # find the middle element
+        midd = (left + right) // 2
+        if midd*midd == x:
+            return midd
+        elif midd*midd < x:
+            
+            # discard left search space
+            left = midd + 1
+            
+            # update result
+            result = midd
+        else:
+            
+            # discard the right search space
+            right = midd - 1
+    return result
+
+print('Square root: ', square_root(num))
